e4f2228c921a73af0cfc0fb94801f82ab3291a79
hereariim <herearii.metuarea@gmail.com>
2022-06-16 11:22:28 +0200

    update

--- src/napari_flower_apple_tree/__init__.py
+++ src/napari_flower_apple_tree/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.0.1"
 
 from ._reader import napari_get_reader
 from ._sample_data import make_sample_data
-from ._widget import ExampleQWidget, example_magic_widget
+from ._widget import widget_image_segmentation, do_image_segmentation
 from ._writer import write_multiple, write_single_image
 
 __all__ = (

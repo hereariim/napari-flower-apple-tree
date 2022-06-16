c08245a0dfb15845d6d22030f857a81ff3e10629
hereariim <herearii.metuarea@gmail.com>
2022-06-16 11:10:45 +0200

    make widget

--- src/napari_flower_apple_tree/_widget.py
+++ src/napari_flower_apple_tree/_widget.py
@@ -15,32 +15,95 @@ if TYPE_CHECKING:
     import napari
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
+from enum import Enum
+import numpy
+import napari
+from napari.types import ImageData
+import cv2
+from magicgui import magic_factory, magicgui
+from skimage import data
+import sys
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+#Etape 1
+import numpy
+import tensorflow as tf
+from tensorflow import keras
+#!pip install focal_loss
+from focal_loss import BinaryFocalLoss
+from tensorflow.keras import backend as K
+import numpy as np
+import cv2
+from skimage.io import imread, imshow, imread_collection, concatenate_images
+from skimage.transform import resize
+from napari.qt.threading import thread_worker
+from napari.utils.notifications import show_info
+from skimage.io import imread
 
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
-
-
+# here's the magicgui!  We also use the additional
+# `call_button` option
 @magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+def widget_image_segmentation(
+    layer: ImageData
+    ) -> ImageData:
+    
+    def redimension(image):
+        X = np.zeros((1,256,256,3),dtype=np.uint8)   
+        size_ = image.shape
+        img = image[:,:,:3]
+        X[0] = resize(img, (256, 256), mode='constant', preserve_range=True)
+        return X,size_
+
+    def dice_coefficient(y_true, y_pred):
+        eps = 1e-6
+        y_true_f = K.flatten(y_true)
+        y_pred_f = K.flatten(y_pred)
+        intersection = K.sum(y_true_f * y_pred_f)
+        return (2. * intersection) / (K.sum(y_true_f * y_true_f) + K.sum(y_pred_f * y_pred_f) + eps) #eps pour éviter la division par 0 
+    
+    image_reshaped,size_ = redimension(layer) #numpy.ndarray
+    
+    #Etape 3 : ii) Segmentation de l'image par l'algorithme U-NET pré-entrainé
+    model_new = tf.keras.models.load_model("best_model_FL_BCE_0_5.h5",custom_objects={'dice_coefficient': dice_coefficient})
+    prediction = model_new.predict(image_reshaped)
+    #Etape 3 : iii) Application du seuil de segmentation optimisé
+    preds_test_t = (prediction > 0.30000000000000004).astype(np.uint8)
+    
+    #preds_test_t = resize(preds_test_t, (3024, 4032), mode='constant', preserve_range=True)
+    #Etape 4 : Output
+    temp=np.squeeze(preds_test_t[0,:,:,0])*255
+    #return cv2.resize(temp, dsize=(3024, 4032)) #2e résultat
+    return cv2.resize(temp, dsize=(size_[1],size_[0])) #1er résultat
 
 
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+@magicgui(call_button="Run segmentation")
+def do_image_segmentation(
+    layer: ImageData
+    ) -> ImageData:
+    
+    def redimension(image):
+        X = np.zeros((1,256,256,3),dtype=np.uint8)   
+        size_ = image.shape
+        img = image[:,:,:3]
+        X[0] = resize(img, (256, 256), mode='constant', preserve_range=True)
+        return X,size_
+
+    def dice_coefficient(y_true, y_pred):
+        eps = 1e-6
+        y_true_f = K.flatten(y_true)
+        y_pred_f = K.flatten(y_pred)
+        intersection = K.sum(y_true_f * y_pred_f)
+        return (2. * intersection) / (K.sum(y_true_f * y_true_f) + K.sum(y_pred_f * y_pred_f) + eps) #eps pour éviter la division par 0 
+    
+    image_reshaped,size_ = redimension(layer) #numpy.ndarray
+    
+    #Etape 3 : ii) Segmentation de l'image par l'algorithme U-NET pré-entrainé
+    model_new = tf.keras.models.load_model("best_model_FL_BCE_0_5.h5",custom_objects={'dice_coefficient': dice_coefficient})
+    prediction = model_new.predict(image_reshaped)
+    #Etape 3 : iii) Application du seuil de segmentation optimisé
+    preds_test_t = (prediction > 0.30000000000000004).astype(np.uint8)
+    
+    #preds_test_t = resize(preds_test_t, (3024, 4032), mode='constant', preserve_range=True)
+    #Etape 4 : Output
+    temp=np.squeeze(preds_test_t[0,:,:,0])*255
+    #return cv2.resize(temp, dsize=(3024, 4032)) #2e résultat
+    return cv2.resize(temp, dsize=(size_[1],size_[0])) #1er résultat

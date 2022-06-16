97f18b9477e7906b431154552be383d38677ac8d
hereariim <herearii.metuarea@gmail.com>
2022-06-16 11:46:51 +0200

    qs

--- src/napari_flower_apple_tree/_widget.py
+++ src/napari_flower_apple_tree/_widget.py
@@ -34,27 +34,6 @@ from napari_plugin_engine import napari_hook_implementation
 from enum import Enum
 from napari.utils.notifications import show_info
 
-
-
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
-
-
 def do_image_segmentation(
     layer: ImageData
     ) -> ImageData:
